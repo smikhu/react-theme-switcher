080325388e34fbc94b8482057764c2e32cbdef32
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-19 20:19:01 -0400

    theme buttons and styled components created and imported

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import { GlobalStyles } from './components/styles/Global';
 import { Header } from './components/styles/Header.styled';
 import { Footer } from './components/styles/Footer.styled';
 import Quotes from './components/Quotes';
+import { ThemeContainer, ThemeButton } from './components/styles/ThemeSwitching.styled';
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <Header>Game of Thrones Quotes</Header>
+
+      <ThemeContainer>
+        <span>Themes: </span>
+        <ThemeButton className="light active"></ThemeButton>
+        <ThemeButton className="dark"></ThemeButton>
+        <ThemeButton className="blue"></ThemeButton>
+        <ThemeButton className="green"></ThemeButton>
+        <ThemeButton className="brown"></ThemeButton>
+        <ThemeButton className="pink"></ThemeButton>
+      </ThemeContainer>
+
+      <Quotes />
 
       <Footer>
         <p>
